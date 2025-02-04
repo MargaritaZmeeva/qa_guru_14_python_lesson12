e7170fe600191fdad1f0b5826ede646b8ef17038
margaritazmeeva <margo.zmeeva23@gmail.com>
2025-02-04 15:53:08 +0300

    extract browser to fixture v4

--- conftest.py
+++ conftest.py
@@ -1,63 +1,56 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selene import Browser, Config
-#
-# from utils import attach
-#
-#
-# @pytest.fixture(scope='function')
-# def setup_chrome():
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "100.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": False
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#
-#     driver = webdriver.Remote(
-#         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#     browser = Browser(Config(driver))
-#
-#     yield browser
-#
-#     attach.add_html(browser)
-#     attach.add_screenshot()
-#     attach.add_html(browser)
-#     attach.add_video(browser)
+import os
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
+from dotenv import load_dotenv
+
 from utils import attach
 
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
 @pytest.fixture(scope='function')
-def setup_chrome():
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "100.0")
-    options.set_capability("selenoid:options", {
-        "enableVNC": True,
-        "enableVideo": False
-    })
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver=driver))
+    browser = Browser(Config(driver))
 
-    yield browser  # Передаем объект browser в тест
+    yield browser
 
-    # Добавление вложений перед закрытием браузера
     attach.add_html(browser)
-    attach.add_screenshot()
-
-    driver.quit()  # Закрываем браузер после теста
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
